a8c5d7790f533fa1cd66d559177d351a2f00f881
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-02-27 10:07:08 +0200

    add a `main` function to haskell qsort file

--- haskell/1-qsort.hs
+++ haskell/1-qsort.hs
@@ -3,3 +3,13 @@ qsort f [] = []
 qsort f [x] = [x]
 qsort f (pivot : rest) =
   qsort f (filter (`f` pivot) rest) ++ [pivot] ++ qsort f (filter (not . (`f` pivot)) rest)
+
+main :: IO ()
+main = do
+  putStr "Unsorted Array: "
+  print unsorted_arr
+  putStr "Sorted Array: "
+  print sorted_arr
+  where
+    unsorted_arr = [1, 3, 5, 2, 9, 8, 10]
+    sorted_arr = qsort (<) unsorted_arr
