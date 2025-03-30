cee4675bde9b9f6bcfc061b9ce94a91d07c956e8
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-03-30 09:45:40 +0200

    implement symbolic math data structure and newton's method in haskell

--- haskell/4.autodiff.hs
+++ haskell/4.autodiff.hs
@@ -89,11 +89,89 @@ real (D u _) = u
 
 dual (D _ u') = u'
 
-deriv :: (Num a) => (Dual a -> Dual a) -> a -> a
-deriv f x = dual . f $ D x 1
+data Symbolic
+  = Symbol String
+  | Number Double
+  | Function Symbolic Symbolic
+  | Binary Symbolic String Symbolic
+  deriving (Eq)
 
-derivX :: (Num a) => (Dual a -> Dual a -> Dual a) -> a -> a -> a
-derivX f x y = dual $ f (D x 1) (D y 0)
+instance Show Symbolic where
+  show (Symbol a) = a
+  show (Number a) = show a
+  show (Function f a) = show f ++ "(" ++ show a ++ ")"
+  show (Binary a o b) = "(" ++ show a ++ " " ++ o ++ " " ++ show b ++ ")"
 
-derivY :: (Num a) => (Dual a -> Dual a -> Dual a) -> a -> a -> a
-derivY f x y = dual $ f (D x 0) (D y 1)
+addition a = Binary a "+"
+subtraction a = Binary a "-"
+division a = Binary a "/"
+multiplication a = Binary a "*"
+exponentiation a = Binary a "**"
+
+instance Num Symbolic where
+  fromInteger = Number . fromInteger
+
+  (Number a) + (Number b) = Number (a + b)
+  (Number 0) + a = a
+  a + (Number 0) = a
+  a + b = if a == b then multiplication a 2 else addition a b
+
+  (Number a) - (Number b) = Number (a - b)
+  a - (Number 0) = a
+  a - b = subtraction a b
+
+  (Number 0) * _ = Number 0
+  _ * (Number 0) = Number 0
+  a * (Number 1) = a
+  (Number a) * (Number b) = Number (a * b)
+  a * b = multiplication a b
+
+  abs = Function (Symbol "abs")
+  negate = Function (Symbol "negate")
+  signum = Function (Symbol "signum")
+
+instance Fractional Symbolic where
+  fromRational = Number . fromRational
+
+  (Number a) / (Number b) = Number (a / b)
+  a / b = division a b
+
+  recip = (1 /)
+
+instance Floating Symbolic where
+  (Number 0) ** _ = Number 0
+  _ ** (Number 0) = Number 1
+  a ** (Number 1) = a
+  (Number a) ** (Number b) = Number (a ** b)
+  a ** b = exponentiation a b
+
+  pi = Symbol "pi"
+  exp = exponentiation (Symbol "e")
+  log = Function (Symbol "log")
+  logBase b = Function (Symbol $ "log" ++ show b)
+  sqrt = Function (Symbol "sqrt")
+  sin = Function (Symbol "sin")
+  cos = Function (Symbol "cos")
+  tan = Function (Symbol "tan")
+  asin = Function (Symbol "asin")
+  acos = Function (Symbol "acos")
+  atan = Function (Symbol "atan")
+  sinh = Function (Symbol "sinh")
+  cosh = Function (Symbol "cosh")
+  tanh = Function (Symbol "tanh")
+  asinh = Function (Symbol "asinh")
+  acosh = Function (Symbol "acosh")
+  atanh = Function (Symbol "atanh")
+
+newtonMethod f x = x - fx / f'x
+  where
+    fr = f (D x 1)
+    fx = real fr
+    f'x = dual fr
+
+newtonMethodIteration _ x 0 = x
+newtonMethodIteration f x i = newtonMethodIteration f (newtonMethod f x) (i - 1)
+
+newtonRoot a b = newtonMethodIteration f b 10
+  where
+    f c = c ** D a 0 - D b 0
