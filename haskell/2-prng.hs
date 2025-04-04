8dcc13ccc263660c16421e591e99894e5e605aba
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-04-04 07:57:21 +0200

    implement applicative and functor for Xoshiro256 type

--- haskell/2-prng.hs
+++ haskell/2-prng.hs
@@ -15,45 +15,52 @@ splitMix s = (d, a)
 
 type Xoshiro256State = (Word64, Word64, Word64, Word64)
 
-xoshiroNew :: Word64 -> Xoshiro256State
-xoshiroNew seed = (a, b, c, d)
+xoshiroInitState :: Word64 -> Xoshiro256State
+xoshiroInitState seed = (a, b, c, d)
   where
     (as, a) = splitMix seed
     (bs, b) = splitMix as
     (cs, c) = splitMix bs
     (_, d) = splitMix cs
 
-xoshiroNext :: Xoshiro256State -> (Word64, Xoshiro256State)
-xoshiroNext (a, b, c, d) = (v, (na, nb, nc, nd))
+newtype Xoshiro256 a = Xoshiro256 {xoshiroRun :: Xoshiro256State -> (Xoshiro256State, a)}
+
+instance Functor Xoshiro256 where
+  fmap f a = Xoshiro256 b
+    where
+      b s = (ns, f v)
+        where
+          (ns, v) = xoshiroRun a s
+
+instance Applicative Xoshiro256 where
+  pure x = Xoshiro256 (,x)
+
+  Xoshiro256 a <*> Xoshiro256 b = Xoshiro256 c
+    where
+      c s = (ls, f v)
+        where
+          (ns, f) = a s
+          (ls, v) = b ns
+
+random64 :: Xoshiro256 Word64
+random64 = Xoshiro256 f
   where
-    v = a + rotateL (a + d) 23
+    f (a, b, c, d) = ((na, nb, nc, nd), v)
+      where
+        v = a + rotateL (a + d) 23
 
-    pc = xor c a
-    pd = xor d b
-    nb = xor b pc
-    na = xor a pd
+        pc = xor c a
+        pd = xor d b
+        nb = xor b pc
+        na = xor a pd
 
-    nc = xor pc (shiftL b 17)
+        nc = xor pc (shiftL b 17)
 
-    nd = rotateL pd 45
+        nd = rotateL pd 45
 
-random8 :: Xoshiro256State -> (Word64, Xoshiro256State)
-random8 s = (v `mod` (1 `shiftL` 8 - 1), ns)
-  where
-    (v, ns) = xoshiroNext s
-
-random16 :: Xoshiro256State -> (Word64, Xoshiro256State)
-random16 s = (v `mod` (1 `shiftL` 16 - 1), ns)
-  where
-    (v, ns) = xoshiroNext s
-
-random32 :: Xoshiro256State -> (Word64, Xoshiro256State)
-random32 s = (v `mod` (1 `shiftL` 32 - 1), ns)
-  where
-    (v, ns) = xoshiroNext s
-
-random64 :: Xoshiro256State -> (Word64, Xoshiro256State)
-random64 = xoshiroNext
+random32 = fmap (\x -> mod x (1 `shiftL` 32 - 1)) random64
+random16 = fmap (\x -> mod x (1 `shiftL` 16 - 1)) random64
+random8 = fmap (\x -> mod x (1 `shiftL` 8 - 1)) random64
 
 main :: IO ()
 main = do
@@ -61,11 +68,17 @@ main = do
 
   let systemTime = utcToSystemTime utcTime
 
-  let state = xoshiroNew $ fromInteger $ toInteger $ systemNanoseconds systemTime
+  let state = xoshiroInitState $ fromInteger $ toInteger $ systemNanoseconds systemTime
 
-  let a : b : c : d : _ =
-        map
-          (\f -> fst (f state))
-          [random8, random16, random32, random64]
+  let (last_state, a : b : c : d : _) =
+        xoshiroRun
+          ( sequenceA
+              [ random64,
+                random32,
+                random16,
+                random8
+              ]
+          )
+          state
 
   putStrLn (show a ++ " " ++ show b ++ " " ++ show c ++ " " ++ show d)
